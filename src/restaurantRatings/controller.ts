7b9906ff97446ea2b83136e97f662f8ecbbc9a4a
Daniel Somerfield <daniel.somerfield@measurabl.com>
2023-04-01 14:13:47 -0700

    Minimal controller unit test $minimal-controller-unit-test

--- src/restaurantRatings/controller.ts
+++ src/restaurantRatings/controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 // <codeFragment name="controller-stub">
 export const createTopRatedHandler = () => {
-  return async (request: Request, response: Response) => {};
+  return async (request: Request, response: Response) => {
+    response.status(200).contentType("application/json").send({});
+  };
 };
 // </codeFragment>
