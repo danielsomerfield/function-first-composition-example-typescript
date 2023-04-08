4b6cab6611fbb48806f9c3292c02c08b40523386
Daniel Somerfield <daniel.somerfield@measurabl.com>
2023-04-08 14:16:48 -0700

    Flesh out topRated module $top-rated

--- src/restaurantRatings/topRated.ts
+++ src/restaurantRatings/topRated.ts
@@ -1,16 +1,70 @@
 // <codeFragment name="top-rated">
-interface Dependencies {}
+interface Dependencies { // ref-dependencies
+  findRatingsByRestaurant: (city: string) => Promise<RatingsByRestaurant[]>;
+  calculateRatingForRestaurant: (ratings: RatingsByRestaurant) => number;
+}
 
+interface OverallRating { // ref-overall-ratings
+  restaurantId: string;
+  rating: number;
+}
+
+interface RestaurantRating { // ref-extract-types
+  rating: Rating;
+}
+
+interface RatingsByRestaurant {
+  restaurantId: string;
+  ratings: RestaurantRating[];
+}
 
 export const create = (dependencies: Dependencies) => { // ref-function-factory
-  return async (city: string): Promise<Restaurant[]> => [];
-}; // end-ref-function-factory
+  const calculateRatings = (
+    ratingsByRestaurant: RatingsByRestaurant[],
+    calculateRatingForRestaurant: (ratings: RatingsByRestaurant) => number,
+  ): OverallRating[] =>
+    ratingsByRestaurant.map(ratings => {
+      return {
+        restaurantId: ratings.restaurantId,
+        rating: calculateRatingForRestaurant(ratings),
+      };
+    });
 
-interface Restaurant { // ref-extract-restaurant
+  const getTopRestaurants = async (city: string): Promise<Restaurant[]> => {
+    const { findRatingsByRestaurant, calculateRatingForRestaurant } =
+      dependencies;
+
+    const ratingsByRestaurant = await findRatingsByRestaurant(city);
+
+    const overallRatings = calculateRatings(
+      ratingsByRestaurant,
+      calculateRatingForRestaurant,
+    );
+
+    const toRestaurant = (r: OverallRating) => ({
+      id: r.restaurantId,
+    });
+
+    return sortByOverallRating(overallRatings).map(r => {
+      return toRestaurant(r);
+    });
+  };
+
+  const sortByOverallRating = (overallRatings: OverallRating[]) =>
+    overallRatings.sort((a, b) => b.rating - a.rating);
+
+  return getTopRestaurants;
+};
+
+//SNIP ..
+
+// </codeFragment>
+
+interface Restaurant {
   id: string;
-}  // end-ref-extract-restaurant
+}
 
-export const rating = { // ref-extract-rating
+export const rating = {
   EXCELLENT: 2,
   ABOVE_AVERAGE: 1,
   AVERAGE: 0,
@@ -18,5 +72,4 @@ export const rating = { // ref-extract-rating
   TERRIBLE: -2,
 } as const;
 
-export type Rating = keyof typeof rating; // end-ref-extract-rating
-// </codeFragment>
+export type Rating = keyof typeof rating;
